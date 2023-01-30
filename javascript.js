64179d9b3dfeba08beeae232013ea1463d02b876
Melva <78463026+melsabiti@users.noreply.github.com>
2023-01-30 13:50:04 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,23 @@
+// --- Directions
+// Check to see if two provided strings are anagrams of eachother.
+// One string is an anagram of another if it uses the same characters
+// in the same quantity. Only consider characters, not spaces
+// or punctuation.  Consider capital letters to be the same as lower case
+// --- Examples
+//   anagrams('rail safety', 'fairy tales') --> True
+//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+//   anagrams('Hi there', 'Bye there') --> False
+
+
+//I did not see a provided string unless the provided string was the strings inside the examples. 
+
+
+function validAnagram(arr1, arr2) {
+
+}
+
+validAnagram('rail safety', 'fairy tales')
+validAnagram('RAIL! SAFETY!', 'fairy tales')
+validAnagram('Hi there', 'Bye there')
+
+console.log(validAnagram)
